fc9bcf3e3e244401b48cb1b8e6c9afa946524271
petrosFr <43893821+petrosFr@users.noreply.github.com>
2018-10-17 17:29:25 +0200

    Update script2.php

--- script2.php
+++ script2.php
@@ -1,8 +1,8 @@
 <?php
 $v1=$_POST["classname"];
 $v2=$_POST["threshold"];
-$pngname = "pictures_uml/CModel_".$v1."_".$v2.".png";
-$generatePhoto = "/usr/lib64/jvm/java-1.8.0-openjdk-1.8.0/jre/bin/java -jar /etudiants/deptinfo/p/pari_p1/workspace/linked_itemset_sub16/uml.jar pictures_uml/CModel_"+v1+"_"+v2+"_mod.txt";
+$pngname = "pictures_uml/CModel_".$v1."_".$v2."_modW.png";
+$generatePhoto = "/usr/lib64/jvm/java-1.8.0-openjdk-1.8.0/jre/bin/java -jar plantuml.jar pictures_uml/CModel_".$v1."_".$v2."_modW.txt";
 $output = shell_exec($generatePhoto);
 echo "<img src=".$pngname." alt=\"Unknown value\">";
 ?>
